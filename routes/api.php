e43a5815ba9ee80347f7187e55665fc9ae977b84
Togsaaay <coolestjanith01@gmail.com>
2025-04-03 10:35:25 +0800

    midterm

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProductControllers;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('products', ProductControllers::class);
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
